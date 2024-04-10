a7f35728fdd5e00e80a3e573e3078cafcda0d5c1
ss37939 <ss37939@georgiasouthern.edu>
2024-04-10 15:53:35 -0400

    Adding more recipes!
    Don't forget to comment/uncomment what you need.

--- cookbook.py
+++ cookbook.py
@@ -45,14 +45,17 @@ data_insertion_statements = {
     "ingredients": [
         #"""INSERT INTO `ingredients` VALUES (3,'garlic'),(4,'olive oil'),(1,'pasta'),(5,'sugar'),(2,'tomato')"""
         #"""INSERT INTO `ingredients` VALUES (6,'hot sauce'),(7,'cheese'),(8,'beef'),(9,'pork'),(10,'chicken'),(11,'noodles'),(12,'sausage'),(13,'marinara sauce'),(14,'eggplant'),(15,'lettuce'),(16,'croutons'),(17,'ceasar dressing'),(18,'dough')"""
+        """INSERT INTO `ingredients` VALUES (19,'bell pepper'),(20,'broccoli'),(21,'soy sauce'),(22,'flour'),(23,'butter'),(24, 'eggs'),(25, 'milk'),(26, 'baking powder'),(27, 'vanilla extract'),(28, 'salt'),(29, 'pepper'),(30, 'garlic powder'),(31, 'paprika'),(32, 'vegetable oil'),(33, 'onion'),(34, 'onion powder'),(35, 'tomato paste'),(36, 'parsely'),(37, 'basil leaves'),(38, 'carrots'),(39, 'celery'),(40, 'potatoes'),(41, 'taco seasoning'),(42, 'tortillas'),(43, 'sour cream'),(44, 'baking soda'),(45, 'chocolate chips'),(46, 'mushrooms'),(47, 'worcestershire sauce')"""
     ],
     "cookid": [
         #"""INSERT INTO `cookid` VALUES (1,1),(2,1),(1,2),(1,3),(2,3),(1,4),(2,4),(3,5)"""
         #"""INSERT INTO `cookid` VALUES (4,6),(5,8),(6,7),(7,9),(4,10),(8,3),(8,7),(8,11),(8,8),(8,12),(8,13),(9,11),(9,13),(9,3),(10,14),(10,13),(10,7),(11,15),(11,7),(11,4),(11,16),(11,17),(12,13),(12,18),(12,7),(12,4)"""
+        #"""INSERT INTO `cookid` VALUES (2,20),(2,21),(2,10),(3,22),(3,23),(3,24),(3,25),(3,26),(3,27),(3,28),(4,23),(4,22),(4,28),(4,29),(4,30),(4,31),(4,32),(5,28),(5,29),(5,32),(5,33),(6,1),(6,23),(6,25),(6,28),(6,29),(7,28),(7,29),(7,30),(7,31),(7,32),(7,34),(8,28),(8,29),(8,24),(8,35),(8,36),(8,33),(9,28),(9,29),(10,28),(10,24),(10,37),(12,28),(12,29),(12,37),(13,28),(13,29),(13,3),(13,2),(13,33),(13,38),(13,39),(13,40),(14,8),(14,41),(14,42),(14,15),(14,2),(14,7),(14,43),(15,22),(15,23),(15,5),(15,24),(15,27),(15,28),(15,44),(15,45),(16,8),(16,33),(16,46),(16,43),(16,47),(16,22),(16,23),(16,11),(17,22),(17,25),(17,24),(17,5),(17,26),(17,28),(17,23)"""
     ],
     "recipes": [
         #"""INSERT INTO `recipes` VALUES (3,'Cake'),(2,'Chicken Stir-Fry'),(1,'Pasta with Tomato Sauce')"""
         #"""INSERT INTO `recipes` VALUES (4,'Hot Wings'),(5,'Hamburger'),(6,'Mac and Cheese'),(7,'Pork Chops'),(8,'Lasagna'),(9,'Spaghetti'),(10,'Eggplant Parmesan'),(11,'Ceasar Salad'),(12,'Margherita Pizza')"""
+        """INSERT INTO `recipes` VALUES (13,'vegetable soup'),(14,'beef taco'),(15,'chocolate chip cookies'),(16,'beef stroganoff'),(17,'pancakes')"""
     ]
 }
 
